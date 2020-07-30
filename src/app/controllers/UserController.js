da49ed885d913151021dfaf74a6eaa44d6bb047e
ViniciusGM <vinicius.aguiar@orgsystem.com.br>
2020-07-30 17:17:29 -0300

    Mudanças no back

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from "yup";
 import User from "../models/User";
+import File from "../models/File";
 
 class UserController {
   async store(req, res) {
@@ -47,20 +48,19 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
+    // Prevents duplicating e-mails
     if (email !== user.email) {
-      const userExists = await User.findOne({
-        where: { email },
-      });
-      if (userExists) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists)
         return res.status(400).json({ error: "User already exists" });
-      }
     }
 
-    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+    if (oldPassword && !(await user.checkPassword(oldPassword)))
       return res.status(401).json({ error: "Password does not match" });
-    }
 
     await user.update(req.body);
+
     const { id, name, avatar } = await User.findByPk(req.userId, {
       include: [
         {
@@ -71,7 +71,12 @@ class UserController {
       ],
     });
 
-    return res.json({ id, name, email, avatar });
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
   }
 }
 
